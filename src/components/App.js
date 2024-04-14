6678bd03118c23c8cea0dd6296570be11e5ca3af
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-14 11:44:51 +0530

    count

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,14 @@
+
 import React from "react";
 import './../styles/App.css';
-import { useState,useEffect } from "react";
-
+import Sum from "./Sum";
 const App = () => {
-  const [numbers, setNumbers] = useState([]);
-  const [totalSum, setTotalSum] = useState(0);
-
-  useEffect(() => {
-    const calculateSum = async () => {
-      let sum = 0;
-      for (const num of numbers) {
-        sum += num;
-      }
-      setTotalSum(sum);
-    };
-    calculateSum();
-  }, [numbers]);
-
-  const handleNumberInputChange = (event) => {
-    const inputValue = parseInt(event.target.value);
-    setNumbers([...numbers, inputValue]);
-  };
   return (
     <div>
-        <h2>Sum Calculator</h2>
-      <input type="number" onChange={handleNumberInputChange}/>
-      <div>
-        <p>Sum: {totalSum}</p>
-      </div>
+        {/* Do not remove the main div */}
+        <Sum />
     </div>
   )
 }
+
 export default App
